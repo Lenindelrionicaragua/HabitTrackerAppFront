f0897356aaf3857d8d60c44b13b7d8bdbf547029
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-24 10:25:24 +0200

    create credentialsContext.js file

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import React from "react";
-// import "@expo/metro-runtime";
-// import { registerRootComponent } from "expo";
-// React Navigation Stack
+import React, { useState, useEffect } from "react";
+import { logError } from "./util/logging";
 import RootStack from "./navigators/RootStack";
+import AppLoading from "expo-app-loading";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const [appReady, setAppReady] = useState(false);
+  const [storedCredentials, setStoredCredentials] = useState("");
+
+  useEffect(() => {
+    checkLoginCredentials();
+  }, []);
+
+  const checkLoginCredentials = () => {
+    AsyncStorage.getItem("zenTimerCredentials")
+      .then(result => {
+        if (result !== null) {
+          setStoredCredentials(JSON.parse(result));
+        } else {
+          setStoredCredentials(null);
+        }
+        setAppReady(true);
+      })
+      .catch(error => logError(error));
+  };
+
+  if (!appReady) {
+    return (
+      <AppLoading
+        startAsync={checkLoginCredentials}
+        onFinish={() => setAppReady(true)}
+        onError={logError}
+      />
+    );
+  }
+
   return <RootStack testID="root-stack" />;
 }
-
-// registerRootComponent(App);
---
+++ __mocks__/AsyncStorage.js
@@ -0,0 +1,10 @@
+const mockAsyncStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  getAllKeys: jest.fn(),
+  mergeItem: jest.fn()
+};
+
+export default mockAsyncStorage;
