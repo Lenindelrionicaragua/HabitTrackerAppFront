9f125a60f07922fa8dc49998f19f0e5f9d9dc6a3
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-24 10:57:01 +0200

    update RootStack to handle the user expericen if there is credential or not

--- navigators/RootStack.js
+++ navigators/RootStack.js
@@ -7,45 +7,56 @@ import { Colors } from "../styles/AppStyles";
 import LoginScreen from "./../screens/LoginScreen/LoginScreen";
 import SignupScreen from "./../screens/SignupScreen/SignupScreen";
 import WelcomeScreen from "./../screens/WelcomeScreen/WelcomeScreen";
+// credentials context
+import { CredentialsContext } from "../context/credentialsContext";
 
 const { white, orange, grey, yellow, lightGrey, black } = Colors;
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyled: {
-            backgroundColor: "transparent",
-          },
-          headerTintColor: lightGrey,
-          headerTransparent: true,
-          headerTitle: "",
-          headerLeftContainerStyle: {
-            paddingLeft: 20,
-          },
-        }}
-        initialRouteName="Login"
-      >
-        <Stack.Screen
-          name="LoginScreen"
-          component={LoginScreen}
-          testID="login-screen"
-        />
-        <Stack.Screen
-          options={{ headerTintColor: grey }}
-          name="SignupScreen"
-          component={SignupScreen}
-          testID="signup-screen"
-        />
-        <Stack.Screen
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-          testID="welcome-screen"
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CredentialsContext.Consumer>
+      {({ storedCredentials }) => (
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyled: {
+                backgroundColor: "transparent"
+              },
+              headerTintColor: lightGrey,
+              headerTransparent: true,
+              headerTitle: "",
+              headerLeftContainerStyle: {
+                paddingLeft: 20
+              }
+            }}
+            initialRouteName="Login"
+          >
+            {storedCredentials ? (
+              <Stack.Screen
+                name="WelcomeScreen"
+                component={WelcomeScreen}
+                testID="welcome-screen"
+              />
+            ) : (
+              <>
+                <Stack.Screen
+                  name="LoginScreen"
+                  component={LoginScreen}
+                  testID="login-screen"
+                />
+                <Stack.Screen
+                  options={{ headerTintColor: grey }}
+                  name="SignupScreen"
+                  component={SignupScreen}
+                  testID="signup-screen"
+                />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+    </CredentialsContext.Consumer>
   );
 };
 
