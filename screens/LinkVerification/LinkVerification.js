01a96194695c70ad1e1ceb9c6f9c12a62629b1f2
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-28 20:26:33 +0200

    add icon and styles for linkVerificationScreen

--- screens/LinkVerification/LinkVerification.js
+++ screens/LinkVerification/LinkVerification.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { Colors } from "./../../styles/AppStyles";
+import {
+  StyledContainer,
+  TopContainer,
+  BottomContainer,
+  IconBackGround
+} from "./LinkVerificationStyles";
+// icon
+import { Octicons } from "@expo/vector-icons";
+
+// Colors
+const { white, orange, grey, yellow, lightGrey, black, lightGreen } = Colors;
 
 const LinkVerification = () => {
-  return <></>;
+  return (
+    <StyledContainer style={{ alignItems: "center" }}>
+      <TopContainer>
+        <IconBackGround>
+          <StatusBar style="dark" />{" "}
+          <Octicons name="mail" size={125} color={orange} />
+        </IconBackGround>
+      </TopContainer>
+      <BottomContainer></BottomContainer>
+    </StyledContainer>
+  );
 };
 
 export default LinkVerification;
