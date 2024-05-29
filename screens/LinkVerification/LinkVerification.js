a1926933c1a2a6966fe38aabccaa3b7836e5e132
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-29 13:10:48 +0200

    add send it button and resend link

--- screens/LinkVerification/LinkVerification.js
+++ screens/LinkVerification/LinkVerification.js
@@ -5,24 +5,55 @@ import {
   StyledContainer,
   TopContainer,
   BottomContainer,
-  IconBackGround
+  IconBackGround,
+  PageTitle,
+  InfoText,
+  EmphasizeText,
+  StyledButton,
+  ButtonText,
+  InlineGroup,
+  TextLink,
+  TextLinkContent
 } from "./LinkVerificationStyles";
 // icon
-import { Octicons } from "@expo/vector-icons";
-
+import { Octicons, Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
 // Colors
-const { white, orange, grey, yellow, lightGrey, black, lightGreen } = Colors;
+const { white, orange, grey, yellow, lightGrey, black } = Colors;
 
 const LinkVerification = () => {
   return (
     <StyledContainer style={{ alignItems: "center" }}>
       <TopContainer>
         <IconBackGround>
-          <StatusBar style="dark" />{" "}
+          <StatusBar style="dark" />
           <Octicons name="mail" size={125} color={orange} />
         </IconBackGround>
       </TopContainer>
-      <BottomContainer></BottomContainer>
+      <BottomContainer>
+        <PageTitle style={{ fontSize: 25 }}>Account Verification</PageTitle>
+        <InfoText>
+          We will sent you an email to verify your account.
+          <EmphasizeText>{`test.riodeluzcreativos@gmail.com`}</EmphasizeText>
+        </InfoText>
+        <StyledButton onPress={() => {}} style={{ flexDirection: "row" }}>
+          <ButtonText>Send it</ButtonText>
+          <Ionicons name="arrow-forward-circle" size={25} color={white} />
+        </StyledButton>
+        <View>
+          <InlineGroup>
+            <InfoText>Didn't receive the email?</InfoText>
+            <TextLink onPress={() => {}}>
+              <TextLinkContent style={{ textDecorationLine: "underline" }}>
+                Resend
+              </TextLinkContent>
+            </TextLink>
+          </InlineGroup>
+          <InfoText>
+            in <EmphasizeText>{`20`}</EmphasizeText> second(s)
+          </InfoText>
+        </View>
+      </BottomContainer>
     </StyledContainer>
   );
 };
