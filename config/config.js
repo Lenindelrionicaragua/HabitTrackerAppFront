c36f753dad66a4bd05968dfa84af6a5bd4c565c9
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-06 13:16:34 +0200

    delete config.js file an create sahred.js file to handle de baseApiUrl

--- config/config.js
+++
@@ -1,5 +0,0 @@
-import { API_URL_DEVELOPMENT, API_URL_PRODUCTION } from "@env";
-
-const environment = process.env.NODE_ENV || "development";
-export const baseApiUrl =
-  environment === "development" ? API_URL_DEVELOPMENT : API_URL_PRODUCTION;
