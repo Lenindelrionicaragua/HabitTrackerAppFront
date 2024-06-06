680e8b394a56773212c90daeba7075736dce5c1e
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-06 13:50:58 +0200

    change the manage of urls in loginScreen, welcomScreen and signupScreen, now, we decide wich url use, in the shared.js file

--- component/Shared/shared.js
+++ component/Shared/shared.js
@@ -1,3 +1,3 @@
 export const baseApiUrl =
-  "https://zen-timer-app-server-7f9db58def4c.herokuapp.com/api/auth/sign-up";
-// "http://192.168.178.182:3000/api/auth/sign-up";
+  "https://zen-timer-app-server-7f9db58def4c.herokuapp.com/api";
+// "http://192.168.178.182:3000/api"; // For test android need ip instead of localHost
