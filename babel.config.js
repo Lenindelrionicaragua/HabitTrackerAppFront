7f4603f41edcd4325821647814bcfaeb6a4acf33
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-15 16:47:31 +0200

    Inicialización del proyecto

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,26 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const presets = ["babel-preset-expo"];
+  const plugins = [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false
+      }
+    ],
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }]
+  ];
+
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blocklist: null,
-          allowlist: null,
-          safe: false,
-          allowUndefined: true,
-          verbose: false
-        }
-      ]
-    ]
+    presets,
+    plugins
   };
 };
