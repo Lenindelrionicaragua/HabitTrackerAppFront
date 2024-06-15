a037b53840f5feb59aa23a4f54654c57fd6c39cc
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-15 15:10:08 +0200

    delete metro.config.js y tambien AppEntry.js

--- AppEntry.js
+++ AppEntry.js
@@ -1,4 +1,9 @@
-import { registerRootComponent } from "expo";
+import { AppRegistry } from "react-native";
 import App from "./App";
+import { name as appName } from "./app.json";
 
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById("app-root")
+});
