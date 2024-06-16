72e092646d1c1e4a3f8370de9133544a934e4fc1
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-16 14:19:06 +0200

    deleted webpack y server.js

--- babel.config.js
+++ babel.config.js
@@ -15,8 +15,9 @@ module.exports = function (api) {
         verbose: false
       }
     ],
-    ["@babel/plugin-transform-class-properties", { loose: true }],
-    ["@babel/plugin-transform-private-methods", { loose: true }]
+    "@babel/plugin-transform-class-properties",
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }]
   ];
 
   return {
