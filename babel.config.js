577241138c514f2331dba28da78ea81cd00511ef
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-17 15:54:37 +0200

    expo export: web, android and ios. Success

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const presets = ["babel-preset-expo"];
-  const plugins = [
-    [
-      "module:react-native-dotenv",
-      {
-        moduleName: "@env",
-        path: ".env",
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false
-      }
-    ],
-    "@babel/plugin-transform-class-properties",
-    ["@babel/plugin-transform-private-methods", { loose: true }],
-    ["@babel/plugin-transform-private-property-in-object", { loose: true }]
-  ];
-
   return {
-    presets,
-    plugins
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env"
+        }
+      ]
+    ]
   };
 };
