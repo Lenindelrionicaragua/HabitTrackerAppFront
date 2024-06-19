9203d8b7429471d84a523037a5f369873bf5d17b
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-19 15:17:22 +0200

    add cypress scripts in package.json

--- cypress/e2e/loginScreenTest.cy.js
+++ cypress/e2e/loginScreenTest.cy.js
@@ -2,14 +2,26 @@ describe("LoginScreen", () => {
   it("render the elements of the LoginScreen", () => {
     cy.visit("http://192.168.178.182:8081");
 
+    cy.get('[data-testId="page-logo"]').should("exist");
+
     cy.get('[data-testId="page-title"]')
       .should("exist")
       .should("have.text", "ZenTimer");
+
+    cy.get('[data-testId="sub-title"]')
+      .should("exist")
+      .should("have.text", "Account Login");
 
     cy.get('[data-testId="email-input"]').should("exist");
 
     cy.get('[data-testId="password-input"]').should("exist");
 
-    cy.get('[data-testId="login-styled-button"]').should("exist");
+    cy.get('[data-testId="msg-box"]').should("exist");
+
+    cy.get('[data-testId="line"]').should("exist");
+
+    cy.get('[data-testId="login-styled-button"]')
+      .should("exist")
+      .should("have.text", "login");
   });
 });
