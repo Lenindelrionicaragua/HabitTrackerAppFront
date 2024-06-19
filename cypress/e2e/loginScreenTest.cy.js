0375565027583f07e597de38a5f4a11ef5547358
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-19 17:58:33 +0200

    add cypress test to verify correct rendering of welcomeScreen after a successful Login

--- cypress/e2e/loginScreenTest.cy.js
+++ cypress/e2e/loginScreenTest.cy.js
@@ -88,7 +88,7 @@ describe("LoginScreen", () => {
   //     );
   // });
 
-  it("render WelcomeScreen correctly, when the email and password are correct", () => {
+  it("render all the WelcomeScreen elements correctly, when the email and password are valids", () => {
     cy.visit("http://192.168.178.182:8081");
 
     const testEmail = "usertest@gmail.com";
@@ -98,8 +98,26 @@ describe("LoginScreen", () => {
     cy.get('[data-testId="password-input"]').type(testPassword);
     cy.get('[data-testId="login-styled-button"]').click();
 
+    cy.get('[data-testId="welcome-image"]').should("exist");
+
     cy.get('[data-testId="welcome-title"]')
       .should("exist")
       .and("have.text", "Welcome!");
+
+    cy.get('[data-testId="user-name"]')
+      .should("exist")
+      .and("have.text", "Len Del Rio");
+
+    cy.get('[data-testId="user-email"]')
+      .should("exist")
+      .and("have.text", "usertest@gmail.com");
+
+    cy.get('[data-testId="avatar-image"]').should("exist");
+
+    cy.get('[data-testId="line"]').should("exist");
+
+    cy.get('[data-testId="logout-button-text"]')
+      .should("exist")
+      .and("have.text", "Logout");
   });
 });
