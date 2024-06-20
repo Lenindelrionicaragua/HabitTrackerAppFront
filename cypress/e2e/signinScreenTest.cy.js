79334fced97952f0a3192b0d79a330880bfd7c2b
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-20 13:49:25 +0200

    update unit test for the signupScreen

--- cypress/e2e/signinScreenTest.cy.js
+++ cypress/e2e/signinScreenTest.cy.js
@@ -1,3 +1,32 @@
-decribe("signinScreen", () => {
-  it("Render all the sign-in Screen elements correctly, after clicking in sign-in link", () => {});
+describe("SigninScreen", () => {
+  it("Render all the sign-in Screen elements correctly, after clicking in sign-in link", () => {
+    cy.visit("http://192.168.178.182:8081");
+
+    cy.get('[data-testId="signup-link-content"]').click();
+
+    cy.wait(500);
+
+    cy.get('[data-testId="signup-page-title"]')
+      .should("exist")
+      .should("have.text", "ZenTimer");
+
+    cy.get('[data-testId="signup-page-sub-title"]')
+      .should("exist")
+      .should("have.text", "Account Sign Up");
+
+    //formik
+    cy.get('[data-testId="name"]').should("exist");
+
+    cy.get('[data-testId="email-input"]').should("exist");
+
+    cy.get('[data-testId="date-of-birth"]').should("exist");
+
+    cy.get('[data-testId="password-input"]').should("exist");
+
+    cy.get('[data-testId="confirm-password-input"]').should("exist");
+
+    cy.get('[data-testId="msg-box"]').should("exist");
+
+    cy.get('[data-testId="signup-styled-button"]').should("exist");
+  });
 });
