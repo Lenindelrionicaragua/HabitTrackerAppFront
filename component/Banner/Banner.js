a0a879ca33d10e618869397302be0149ba4dc02a
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-26 18:07:43 +0200

    use dimension from react-native, to fit the SVG element to the width of the screen

--- component/Banner/Banner.js
+++ component/Banner/Banner.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Dimensions
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Svg, { Path } from "react-native-svg";
+import { Colors } from "../../styles/AppStyles";
+
+const { black, orange, white } = Colors;
 
 const Banner = () => {
   const navigation = useNavigation();
+  const { width } = Dimensions.get("window");
+  const { height } = Dimensions.get("window");
+  // Define a condition for mobile devices
+  const isMobile =
+    Platform.OS === "ios" || Platform.OS === "android" || width < 768;
+
+  const svgHeight = isMobile ? "90" : { width };
+  const svgWidth = isMobile ? "100%" : { height };
+  const viewBox = isMobile ? "0 0 1440 120" : "0 0 1440 240";
 
   return (
     <View style={styles.bannerContainer}>
-      <Svg height="100" width="100%" viewBox="0 0 1440 120" style={styles.wave}>
+      <Svg
+        height={svgHeight}
+        width={svgWidth}
+        viewBox={viewBox}
+        style={styles.wave}
+      >
         <Path
+          style={styles.wavePath}
           fill={"green"}
           d="M0,160 C360,320 1080,0 1440,160 L1440,320 L0,320 Z"
         />
@@ -44,32 +69,31 @@ const styles = StyleSheet.create({
     bottom: 0,
     width: "100%",
     overflow: "hidden",
-    backgroundColor: "yellow"
+    backgroundColor: "transparent"
   },
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    backgroundColor: "red",
+    backgroundColor: orange,
     paddingTop: 0,
     padding: 10
   },
   wave: {
-    // position: "absolute",
-    // top: 0
-    paddingTop: 0,
-    width: "100%",
+    marginTop: 0,
+    width: "100%"
+    // backgroundColor: "blue"
+  },
+  wavePath: {
     backgroundColor: "red"
-    // position: "absolute",
-    // bottom: -100
   },
   button: {
     paddingVertical: 10,
     paddingHorizontal: 20,
-    backgroundColor: "orange",
+    backgroundColor: orange,
     borderRadius: 5
   },
   buttonText: {
-    color: "white",
+    color: white,
     fontSize: 16
   }
 });
