bea7ddc1e2700237732b8d9c8829bbbffef53268
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-27 13:45:22 +0200

    create BannerStyles.js

---
+++ component/Banner/BannerStyles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Colors } from "../../styles/AppStyles";
+
+const { black, white } = Colors;
+
+export const BannerContainer = styled(View)`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const ButtonContainer = styled(View)`
+  flex-direction: row;
+  justify-content: space-around;
+  background-color: ${black};
+  padding: 10px;
+`;
+
+export const Button = styled(TouchableOpacity)`
+  padding-vertical: 10px;
+  padding-horizontal: 20px;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+export const ButtonText = styled(Text)`
+  color: ${white};
+  font-size: 12px;
+`;
