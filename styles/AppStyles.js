6c7f3e8504c8fc32d25f50b839cddfbb524d5101
lenin del rio <lenindelrionicaragua@gmail.com>
2024-07-18 16:56:33 +0200

    chore: Update color names to accurately represent their values
    - Renamed color variables to reflect their true colors for clarity and consistency across files
    - Previous color order:
      - white was #3cbc9c (now seaGreen)
      - orange was #ffffff (now white)
      - grey was #ffffff (now infoWhite)
      - yellow was #ecc4d4 (now lightPink)
      - lightGrey was #6c6c8c (now darkGrey)
    - Current colors are seaGreen, white, infoWhite, lightPink, darkGrey, black, skyBlue, and lightGreen
    - Once finalized, color names will be updated to their final versions

--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -1,9 +1,9 @@
 export const Colors = {
-  white: "#3cbc9c",
-  orange: "#ffffff" /* inputs background */,
-  grey: "#ffffff" /* info letter */,
-  yellow: "#ecc4d4",
-  lightGrey: "#6c6c8c" /* background app */,
+  seaGreen: "#3cbc9c",
+  white: "#ffffff" /* inputs background */,
+  infoWhite: "#ffffff" /* info letter */,
+  linkPink: "#ecc4d4",
+  darkGrey: "#6c6c8c" /* background app */,
   black: "#040c0c",
   skyBlue: "#87CEEB",
   lightGreen: `rgba(16, 185, 129, 0.1)`
