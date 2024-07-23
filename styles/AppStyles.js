4b6f8bdc723d115a73399cdda45d0c9f026a459c
lenin del rio <lenindelrionicaragua@gmail.com>
2024-07-23 14:41:00 +0200

    install react-native-linear-gradient

--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -2,7 +2,7 @@ export const Colors = {
   seaGreen: "#3cbc9c",
   white: "#ffffff" /* inputs background */,
   infoWhite: "#ffffff" /* info letter */,
-  lightPink: "#5cbcfc",
+  lightPink: "#ffffff",
   darkGrey: "#6c6c8c" /* background app */,
   black: "#040c0c",
   skyBlue: "#5cbcfc",
