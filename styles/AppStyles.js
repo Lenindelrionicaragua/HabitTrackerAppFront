6720e14fdde8e5d326a6f790c68af654e4864b5d
lenin del rio <lenindelrionicaragua@gmail.com>
2024-07-23 15:35:29 +0200

    style: update color palette and styles for StopwatchScreen
    - Implemented a new color palette across the app.
    - Adjusted styles in StopwatchScreen to reflect the new color scheme.
    - Ensured that all components are aligned with the updated design.

--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -3,7 +3,7 @@ export const Colors = {
   white: "#ffffff" /* inputs background */,
   infoWhite: "#bbcbde" /* info letter */,
   lightPink: "#012382",
-  darkGrey: "#bbcbde" /* background app */,
+  darkGrey: "#fffff" /* background app */,
   black: "#040c0c",
   skyBlue: "#5cbcfc",
   lightGreen: `#1854fc`
