2a6436e3851fde510baa177f442168f8c72bed3c
lenin del rio <lenindelrionicaragua@gmail.com>
2024-07-23 15:12:41 +0200

    chore: update metro.config.js for Expo and configure SVG transformer
    - Removed dependency on @react-native/metro-config to align with Expo setup.
    - Configured react-native-svg-transformer for handling SVG assets in the project.

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const { getDefaultConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
+  const config = getDefaultConfig(__dirname);
 
   const { transformer, resolver } = config;
 
