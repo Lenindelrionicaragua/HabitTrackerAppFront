7de12a73d285ddb88cb3f43dfea5fb5a54a82c50
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-08 20:11:33 +0200

    install expo-notification

--- screens/StopwatchScreen/StopwatchScreen.js
+++ screens/StopwatchScreen/StopwatchScreen.js
@@ -4,8 +4,6 @@ import Svg, { Circle, Rect, Text as SvgText } from "react-native-svg";
 import { Colors } from "../../styles/AppStyles";
 import { clearMessagesAndTimeouts, clearInfoTextAfter } from "../../util/utils";
 import { logInfo, logError } from "../../util/logging";
-import { Audio } from "expo-av";
-import { Asset } from "expo-asset";
 
 import {
   MaterialIcons,
@@ -103,51 +101,7 @@ const StopwatchScreen = () => {
     }
   }, [timeCompleted]);
 
-  useEffect(() => {
-    async function loadAudio() {
-      try {
-        const asset = Asset.fromModule(require("../../assets/alarm_1.mp3"));
-        await asset.downloadAsync();
-        setAlarm(asset);
-      } catch (error) {
-        logError("Error loading audio asset:", error);
-      }
-    }
-
-    loadAudio();
-
-    // Clean up sound when the component is unmounted
-    return () => {
-      if (alarm) {
-        alarm.unloadAsync();
-      }
-    };
-  }, []);
-
-  async function playAlarm() {
-    try {
-      if (alarm) {
-        const { sound } = await Audio.Sound.createAsync({
-          uri: alarm.localUri
-        });
-        setAlarm(sound);
-
-        logInfo("Playing notification Sound");
-        await sound.playAsync();
-
-        sound.setOnPlaybackStatusUpdate(status => {
-          if (status.didJustFinish) {
-            logInfo("Sound has finished playing");
-            sound.unloadAsync();
-          }
-        });
-      } else {
-        logError("Audio asset is not loaded");
-      }
-    } catch (error) {
-      logError("Error playing the notification sound:", error);
-    }
-  }
+  // Alarm notification
 
   // Start button
   const startStopwatch = () => {
