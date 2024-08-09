9a8bb6ce126bc9a7505936cbdaf1ef0f068dd9e6
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-09 21:19:00 +0200

    create DeviceInfoMock

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  return {
+
+  const babelConfig = {
     presets: ["babel-preset-expo"],
     plugins: [
       [
@@ -10,9 +11,27 @@ module.exports = function (api) {
           path: ".env"
         }
       ],
+      [
+        "module-resolver",
+        {
+          alias: {
+            components: "./src/components",
+            screens: "./src/screens",
+            "react-native-device-info$": "./mock/DeviceInfoMock" // Alias para DeviceInfoMock
+          },
+          extensions: [".ios.js", ".android.js", ".js", ".json"]
+        }
+      ],
       "@babel/plugin-proposal-nullish-coalescing-operator",
       "@babel/plugin-proposal-optional-chaining",
       "@babel/plugin-proposal-export-default-from"
     ]
   };
+
+  if (!config.isEjected) {
+    babelConfig.plugins[0][1].alias["react-native-device-info$"] =
+      "./mock/DeviceInfoMock";
+  }
+
+  return babelConfig;
 };
