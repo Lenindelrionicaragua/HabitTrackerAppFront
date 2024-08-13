cde0f5927c893a23aa004860881a7e84d0e2c1b4
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-13 21:34:04 +0200

    modify the test for useCircleParams

--- __test__/screensTest/HookTest/useCircleParams.test.js
+++ __test__/screensTest/HookTest/useCircleParams.test.js
@@ -1,52 +1,6 @@
-import { useState, useEffect } from "react";
 import { renderHook } from "@testing-library/react-hooks";
+import useCircleParams from "../../../hooks/useCircleParams";
 
-// Hook Implementation
-function useCircleParams(elapsedTime, initialTime) {
-  const [circleParams, setCircleParams] = useState({
-    circumference: 0,
-    strokeDashoffset: 0
-  });
-
-  useEffect(() => {
-    const calculateCircleParams = (elapsedTime, initialTime) => {
-      const radius = 150;
-      const circumference = 2 * Math.PI * radius;
-
-      const effectiveElapsedTime = Number.isFinite(elapsedTime)
-        ? elapsedTime
-        : 0;
-      const effectiveInitialTime = Number.isFinite(initialTime)
-        ? initialTime
-        : 0;
-
-      if (effectiveElapsedTime <= 0) {
-        return { circumference, strokeDashoffset: circumference };
-      }
-
-      const timeFraction = Math.min(
-        effectiveElapsedTime / effectiveInitialTime,
-        1
-      );
-
-      let strokeDashoffset = circumference * (1 - timeFraction);
-
-      strokeDashoffset = Math.max(
-        0,
-        Math.round(strokeDashoffset * 1000) / 1000
-      );
-
-      return { circumference, strokeDashoffset };
-    };
-
-    const params = calculateCircleParams(elapsedTime, initialTime);
-    setCircleParams(params);
-  }, [elapsedTime, initialTime]);
-
-  return circleParams;
-}
-
-// Tests
 describe("useCircleParams", () => {
   test("returns correct values when elapsedTime and initialTime are both valid", () => {
     const { result } = renderHook(() => useCircleParams(50, 100));
