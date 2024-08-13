2f00131df5c2c8095b27419bb4c7b395f15bb766
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-13 21:08:13 +0200

    Add tests for useCircleParams hook including edge cases

--- __test__/screensTest/HookTest/useStopwatchScreen.test.js
+++ __test__/screensTest/HookTest/useStopwatchScreen.test.js
@@ -115,23 +115,19 @@ describe("useStopwatchScreen", () => {
       result.current.startTimer(initialTime);
     });
 
-    // Advance the timer by 10 seconds
     act(() => {
       jest.advanceTimersByTime(10000);
     });
 
-    // Pause the stopwatch
     act(() => {
       result.current.pauseStopwatch();
     });
 
-    // Check if running is set to false
     expect(result.current.running).toBe(false);
 
     const elapsedTimeAfterPause = result.current.elapsedTime;
     const remainingTimeAfterPause = result.current.remainingTime;
 
-    // Advance time by another 10 seconds, which should not affect elapsedTime or remainingTime
     act(() => {
       jest.advanceTimersByTime(10000);
     });
@@ -139,7 +135,6 @@ describe("useStopwatchScreen", () => {
     expect(result.current.elapsedTime).toBe(elapsedTimeAfterPause);
     expect(result.current.remainingTime).toBe(remainingTimeAfterPause);
 
-    // Ensure clearInterval was called exactly once
     expect(clearInterval).toHaveBeenCalledTimes(1);
   });
 
@@ -151,25 +146,20 @@ describe("useStopwatchScreen", () => {
       result.current.startTimer(initialTime);
     });
 
-    // Advance the timer by 10 seconds
     act(() => {
       jest.advanceTimersByTime(10000);
     });
 
-    // Pause the stopwatch
     act(() => {
       result.current.pauseStopwatch();
     });
 
-    // Resume the stopwatch
     act(() => {
       result.current.resumeStopwatch();
     });
 
-    // Check if running is set to true
     expect(result.current.running).toBe(true);
 
-    // Advance time by another 10 seconds
     act(() => {
       jest.advanceTimersByTime(10000);
     });
@@ -182,23 +172,18 @@ describe("useStopwatchScreen", () => {
     const initialTime = 50;
     const { result } = renderHook(() => useStopwatchScreen());
 
-    // Start the timer with the initial time
     act(() => {
       result.current.startTimer(initialTime);
     });
 
-    // Advance the timer by the full duration of the initial time
     act(() => {
-      jest.advanceTimersByTime(initialTime * 1000); // Convert seconds to milliseconds
+      jest.advanceTimersByTime(initialTime * 1000);
     });
 
-    // Verify that the timer is not running anymore
     expect(result.current.running).toBe(false);
 
-    // Check if the timer has completed
     expect(result.current.timeCompleted).toBe(true);
 
-    // Verify that the remaining time is 0 and elapsed time is equal to the initial time
     expect(result.current.remainingTime).toBe(0);
     expect(result.current.elapsedTime).toBe(initialTime);
   });
--- __test__/screensTest/StopwatchScreen.test.js
+++ __test__/screensTest/StopwatchScreen.test.js
@@ -1,84 +1,21 @@
-import { renderHook, act } from "@testing-library/react-hooks";
-import { render, cleanup } from "@testing-library/react";
-import renderer from "react-test-renderer";
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor
+} from "@testing-library/react-native";
 import StopwatchScreen from "../../screens/StopwatchScreen/StopwatchScreen";
 
-// Setup fake timers for Jest
-beforeEach(() => {
-  jest.useFakeTimers();
-});
-
-afterEach(() => {
-  cleanup();
-  jest.runOnlyPendingTimers();
-  jest.useRealTimers();
-});
-
 describe("StopwatchScreen", () => {
-  it("should decrease the time accurately and consistently", () => {
-    const initialTime = 50;
+  test("Should start the timer by pressing the start button", () => {
+    render(<StopwatchScreen />);
 
-    // Render the component and simulate starting the timer
-    const { getByText } = render(<StopwatchScreen />);
-    const { result } = renderHook(() => StopwatchScreen());
+    expect(screen.getByTestId("svg-time-text")).toBeTruthy();
 
-    // Start the timer
-    act(() => {
-      result.current.startTimer(initialTime);
-    });
+    fireEvent.press(screen.getByTestId("start-button"));
 
-    // Simulate 10 seconds passing
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(result.current.remainingTime).toBe(40);
-    expect(result.current.elapsedTime).toBe(10);
-
-    // Simulate another 40 seconds passing
-    act(() => {
-      jest.advanceTimersByTime(40000);
-    });
-
-    expect(result.current.remainingTime).toBe(0);
-    expect(result.current.elapsedTime).toBe(50);
-    expect(result.current.timeCompleted).toBe(true);
-  });
-
-  it("should pause the stopwatch correctly", () => {
-    const initialTime = 50;
-
-    // Render the component and simulate starting the timer
-    const { getByText } = render(<StopwatchScreen />);
-    const { result } = renderHook(() => StopwatchScreen());
-
-    // Start the timer
-    act(() => {
-      result.current.startTimer(initialTime);
-    });
-
-    // Simulate 10 seconds passing
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    // Pause the stopwatch
-    act(() => {
-      result.current.pauseStopwatch();
-    });
-
-    // Verify the stopwatch is paused
-    expect(result.current.running).toBe(false);
-
-    const elapsedTimeAfterPause = result.current.elapsedTime;
-    const remainingTimeAfterPause = result.current.remainingTime;
-
-    // Simulate another 10 seconds passing, should not affect times
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(result.current.elapsedTime).toBe(elapsedTimeAfterPause);
-    expect(result.current.remainingTime).toBe(remainingTimeAfterPause);
+    setTimeout(() => {
+      expect(screen.getByText("00:00:01")).toBeTruthy();
+    }, 1000);
   });
 });
