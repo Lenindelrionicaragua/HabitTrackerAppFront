42b498cac22630aa8f1730d58bd9a01371daa6ff
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-15 16:59:03 +0200

    Updated Jest configuration and Babel setup:
    - Modified  to ensure Jest processes modern JavaScript syntax in dependencies like  and .
    - Added  and  for transforming JavaScript and TypeScript files.
    - Updated Babel configuration to handle modern syntax and TypeScript.
    - Cleared Jest cache and reinstalled dependencies.
    
    Resolved issues with Jest failing to parse modern JavaScript syntax in dependencies.

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,12 @@ module.exports = {
   ],
   transformIgnorePatterns: [
     // "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
-    "node_modules/(?!(react-redux|@reduxjs/toolkit|@react-navigation/.*|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))"
+    "node_modules/(?!(react-redux|@reduxjs/toolkit|@react-native/.*|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native/js-polyfills/.*))"
   ],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
   setupFiles: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "^@env$": "<rootDir>/__mocks__/@env.js"
