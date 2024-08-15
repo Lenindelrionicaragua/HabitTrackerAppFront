df3d5bfb049df865c3128e407ef36c06a716fa35
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-15 16:17:15 +0200

    Added console logs to verify that the Redux chain is properly connected and functioning. The application works as expected now. We need to address the issue with Jest.

--- reducers/counterReducer.js
+++ reducers/counterReducer.js
@@ -5,6 +5,8 @@ const initialState = {
 const SET_TIME = "SET_TIME";
 
 const counterReducer = (state = initialState, action) => {
+  console.log("Reducer called with action:", action);
+  console.log("Previous state:", state);
   switch (action.type) {
     case SET_TIME:
       return {
