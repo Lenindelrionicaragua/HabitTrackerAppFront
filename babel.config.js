5a21ba23708e337945201101c9dc249445aabfa2
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-15 16:59:03 +0200

    Updated Jest configuration and Babel setup:
    - Modified  to ensure Jest processes modern JavaScript syntax in dependencies like  and .
    - Added  and  for transforming JavaScript and TypeScript files.
    - Updated Babel configuration to handle modern syntax and TypeScript.
    - Cleared Jest cache and reinstalled dependencies.
    
    Resolved issues with Jest failing to parse modern JavaScript syntax in dependencies.

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,8 @@ module.exports = function (api) {
       "@babel/plugin-proposal-nullish-coalescing-operator",
       "@babel/plugin-proposal-optional-chaining",
       "@babel/plugin-proposal-export-default-from",
-      "@babel/plugin-transform-modules-commonjs"
+      "@babel/plugin-transform-modules-commonjs",
+      "@babel/plugin-transform-typescript"
     ]
   };
 };
