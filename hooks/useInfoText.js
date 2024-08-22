9ba50bac745035c58122700a88d72d37ab56ef2d
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-22 17:48:12 +0200

    Refactor  hook and update tests

--- hooks/useInfoText.js
+++ hooks/useInfoText.js
@@ -1,30 +1,20 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setInfoText, setResetTimeoutsIds } from "../actions/counterActions";
-import { clearMessagesAndTimeouts } from "../util/messageAndTimeoutHandlers";
 
 const useInfoText = () => {
   const infoText = useSelector(state => state.infoText.infoText);
-  const resetTimeoutsIds = useSelector(
-    state => state.resetTimeoutsIds.resetTimeoutsIds
-  );
   const dispatch = useDispatch();
 
-  const clearPreviousTimeouts = () => {
-    console.log("Clearing previous timeouts");
-    clearMessagesAndTimeouts(
-      resetTimeoutsIds,
-      ids => dispatch(setResetTimeoutsIds(ids)),
-      msg => dispatch(setInfoText(msg))
-    );
+  const clearTimeoutsAndMessage = () => {
+    dispatch(setResetTimeoutsIds([]));
+    dispatch(setInfoText(""));
   };
 
   const setInfoTextWithTimeout = (text, timeout) => {
-    console.log("Setting infoText:", text);
     dispatch(setInfoText(text));
 
     const timeoutId = setTimeout(() => {
-      console.log("Clearing infoText after timeout:", timeout);
       dispatch(setInfoText(""));
     }, timeout);
 
@@ -33,14 +23,14 @@ const useInfoText = () => {
 
   useEffect(() => {
     return () => {
-      clearPreviousTimeouts();
+      clearTimeoutsAndMessage();
     };
   }, [infoText, dispatch]);
 
   return {
     infoText,
     setInfoTextWithTimeout,
-    clearPreviousTimeouts
+    clearTimeoutsAndMessage
   };
 };
 
