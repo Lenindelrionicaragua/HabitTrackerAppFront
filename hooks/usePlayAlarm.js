058ea000c6f9ad3cec6bb05bd3494ae56c202b7f
lenin del rio <lenindelrionicaragua@gmail.com>
2024-08-31 14:19:04 +0200

    create useButtonHandler in the hook folders

--- hooks/usePlayAlarm.js
+++ hooks/usePlayAlarm.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import { Audio } from "expo-av";
+// import { useState } from "react";
+// import { Audio } from "expo-av";
 
-export const usePlayAlarm = (logInfo, logError) => {
-  const [alarm, setAlarm] = useState(null);
+// export const usePlayAlarm = (logInfo, logError) => {
+//   const [alarm, setAlarm] = useState(null);
 
-  const playAlarm = async soundPath => {
-    try {
-      logInfo("Loading Sound");
-      if (alarm) {
-        await alarm.unloadAsync();
-        setAlarm(null);
-      }
+//   const playAlarm = async soundPath => {
+//     try {
+//       logInfo("Loading Sound");
+//       if (alarm) {
+//         await alarm.unloadAsync();
+//         setAlarm(null);
+//       }
 
-      const { sound } = await Audio.Sound.createAsync(soundPath);
-      setAlarm(sound);
+//       const { sound } = await Audio.Sound.createAsync(soundPath);
+//       setAlarm(sound);
 
-      logInfo("Playing notification Sound");
-      await sound.playAsync();
+//       logInfo("Playing notification Sound");
+//       await sound.playAsync();
 
-      sound.setOnPlaybackStatusUpdate(status => {
-        if (status.didJustFinish) {
-          logInfo("Sound has finished playing");
-          sound.unloadAsync();
-        }
-      });
-    } catch (error) {
-      logError("Error playing the notification sound:", error);
-    }
-  };
+//       sound.setOnPlaybackStatusUpdate(status => {
+//         if (status.didJustFinish) {
+//           logInfo("Sound has finished playing");
+//           sound.unloadAsync();
+//         }
+//       });
+//     } catch (error) {
+//       logError("Error playing the notification sound:", error);
+//     }
+//   };
 
-  return { playAlarm, alarm };
-};
+//   return { playAlarm, alarm };
+// };
