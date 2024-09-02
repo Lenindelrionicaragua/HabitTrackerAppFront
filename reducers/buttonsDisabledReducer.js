ae52b2f6b544de441f58f83fb81f030801137da7
lenin del rio <lenindelrionicaragua@gmail.com>
2024-09-02 10:40:50 +0200

    commit before moving to a previous branch to investigate why the web version is failing

--- reducers/buttonsDisabledReducer.js
+++ reducers/buttonsDisabledReducer.js
@@ -5,10 +5,8 @@ const initialButtonsDisabled = {
 const SET_BUTTONS_DISABLED = "SET_BUTTONS_DISABLED";
 
 const buttonsDisabledReducer = (state = initialButtonsDisabled, action) => {
-  console.log("Action received:", action);
   switch (action.type) {
     case SET_BUTTONS_DISABLED:
-      console.log("SET_BUTTONS_DISABLED payload:", action.payload);
       return {
         ...state,
         buttonsDisabled: action.payload
