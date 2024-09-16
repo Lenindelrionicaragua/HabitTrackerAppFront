23f94c056643b1ce9eced54d262cc5bb735f03a9
Len Del Río <142052112+Lenindelrionicaragua@users.noreply.github.com>
2024-09-16 17:04:11 +0200

    Update SharedUrl.js

--- component/Shared/SharedUrl.js
+++ component/Shared/SharedUrl.js
@@ -1,5 +1,4 @@
 import {
-  NODE_ENV,
   API_URL_DEVELOPMENT,
   API_URL_PRODUCTION,
   EXPO_CLIENT_ID,
@@ -12,14 +11,8 @@ import { logInfo } from "../../util/logging";
 
 export const baseApiUrl = API_URL_PRODUCTION;
 
-// export const baseApiUrl =
-//   NODE_ENV === "production" ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
-
 export const expoClientId = EXPO_CLIENT_ID;
 export const iosClientId = IOS_CLIENT_ID;
 export const androidClientId = ANDROID_CLIENT_ID;
 export const webClientId = WEB_CLIENT_ID;
 
-logInfo(`NODE_ENV: ${NODE_ENV}`);
-// logInfo(`API_URL_DEVELOPMENT: ${API_URL_DEVELOPMENT}`);
-logInfo(`API_URL_PRODUCTION: ${API_URL_PRODUCTION}`);
