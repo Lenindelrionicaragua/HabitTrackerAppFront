046fa8a12951d12728c8a6b4d3b3e93a2a746cca
lenin del rio <lenindelrionicaragua@gmail.com>
2024-09-16 16:11:58 +0200

    commit before pull

--- component/Shared/SharedUrl.js
+++ component/Shared/SharedUrl.js
@@ -11,7 +11,7 @@ import {
 import { logInfo } from "../../util/logging";
 
 export const baseApiUrl =
-  NODE_ENV !== "production" ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
+  NODE_ENV === "production" ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
 
 export const expoClientId = EXPO_CLIENT_ID;
 export const iosClientId = IOS_CLIENT_ID;
@@ -22,7 +22,7 @@ logInfo(`NODE_ENV: ${NODE_ENV}`);
 logInfo(`API_URL_DEVELOPMENT: ${API_URL_DEVELOPMENT}`);
 logInfo(`API_URL_PRODUCTION: ${API_URL_PRODUCTION}`);
 
-// logInfo(`EXPO_CLIENT_ID: ${EXPO_CLIENT_ID}`);
-// logInfo(`IOS_CLIENT_ID: ${IOS_CLIENT_ID}`);
-// logInfo(`ANDROID_CLIENT_ID: ${ANDROID_CLIENT_ID}`);
-// logInfo(`WEB_CLIENT_ID: ${WEB_CLIENT_ID}`);
+logInfo(`EXPO_CLIENT_ID: ${EXPO_CLIENT_ID}`);
+logInfo(`IOS_CLIENT_ID: ${IOS_CLIENT_ID}`);
+logInfo(`ANDROID_CLIENT_ID: ${ANDROID_CLIENT_ID}`);
+logInfo(`WEB_CLIENT_ID: ${WEB_CLIENT_ID}`);
