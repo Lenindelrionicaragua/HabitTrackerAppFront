c6edb80046456e5e8a92546950a94ed3df533d44
lenin del rio <lenindelrionicaragua@gmail.com>
2024-09-20 16:27:27 +0200

    uninstall @testing-library/react-hooks --legacy-peer-deps

--- __test__/hookTest/usePlayAlarm.test.js
+++ __test__/hookTest/usePlayAlarm.test.js
@@ -1,5 +1,5 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-import { Audio } from "expo-av";
+// import { Audio } from "expo-av";
 import { usePlayAlarm } from "../../hooks/usePlayAlarm";
 
 jest.mock("expo-av", () => ({
