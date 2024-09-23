3fc08e8c466101d950ad9ae7d18128b6aae09053
lenin del rio <lenindelrionicaragua@gmail.com>
2024-09-23 18:02:42 +0200

    Implementing useFetch Hook
    All tests for the useFetch hook have passed successfully:
    
    should handle successful GET request
    should handle failed GET request
    should handle unsuccessful response
    should handle null request parameters
    should handle invalid URL
    should call cancelFetch
    should return cancelFetch function
    We can now proceed with the implementation of this custom hook in the screens.

--- __test__/hookTest/useFetch.test.js
+++ __test__/hookTest/useFetch.test.js
@@ -3,8 +3,8 @@ import useFetch from "../../hooks/useFetch";
 
 describe("useFetch Hook", () => {
   beforeEach(() => {
-    global.fetch = jest.fn();
     jest.clearAllMocks();
+    global.fetch = jest.fn();
   });
 
   afterEach(() => {
@@ -34,32 +34,84 @@ describe("useFetch Hook", () => {
     expect(result.current.error).toBe(null);
   });
 
-  it("should call cancelFetch to abort the fetch request", async () => {
-    fetch.mockImplementationOnce((url, options) => {
-      return new Promise((_, reject) => {
-        options.signal.onabort = () => {
-          reject(new Error("Fetch was canceled"));
-        };
-      });
-    });
+  it("should handle failed GET request", async () => {
+    fetch.mockRejectedValueOnce(new Error("Network Error"));
 
     const onReceived = jest.fn();
-    const { result } = renderHook(() => useFetch("/test-route", onReceived));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch("/test-route", onReceived)
+    );
 
     act(() => {
       result.current.performFetch();
     });
 
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.error).toBeInstanceOf(Error);
+    expect(result.current.error.message).toBe("Network Error");
+  });
+
+  it("should handle unsuccessful response", async () => {
+    const mockResponse = { success: false, msg: "Error message" };
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(mockResponse)
+    });
+
+    const onReceived = jest.fn();
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch("/test-route", onReceived)
+    );
+
+    act(() => {
+      result.current.performFetch();
+    });
+
+    await waitForNextUpdate();
+
+    expect(onReceived).not.toHaveBeenCalled();
+    expect(result.current.error).toBe("Error message");
+  });
+
+  it("should handle null request parameters", async () => {
+    const { result } = renderHook(() => useFetch(null, jest.fn()));
+
+    expect(() => result.current.performFetch()).toThrow(
+      "Invalid route provided"
+    );
+  });
+
+  it("should handle invalid URL", async () => {
+    const { result } = renderHook(() => useFetch("invalid-url", jest.fn()));
+
+    await act(async () => {
+      await result.current.performFetch();
+    });
+
+    expect(result.current.error).toBe("Invalid URL");
+  });
+
+  it("should call cancelFetch", () => {
+    const onReceived = jest.fn();
+    const { result } = renderHook(() => useFetch("/test-route", onReceived));
+    const spyCancelFetch = jest.spyOn(result.current, "cancelFetch");
+
     act(() => {
       result.current.cancelFetch();
     });
 
-    await act(async () => {
-      await new Promise(resolve => setImmediate(resolve));
-    });
+    expect(spyCancelFetch).toHaveBeenCalled();
 
-    expect(result.current.isLoading).toBe(false);
-    expect(result.current.error).toBeInstanceOf(Error);
-    expect(result.current.error.message).toMatch("Fetch was canceled");
+    spyCancelFetch.mockRestore();
+  });
+
+  it("should return cancelFetch function", () => {
+    const onReceived = jest.fn();
+
+    const { result } = renderHook(() => useFetch("/test-route", onReceived));
+
+    expect(typeof result.current.cancelFetch).toBe("function");
   });
 });
