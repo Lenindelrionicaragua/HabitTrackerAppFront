1033602c93449e059179d86a5d167ef562f33ea0
lenin del rio <lenindelrionicaragua@gmail.com>
2024-11-11 12:50:19 +0100

    first test for the useHabitCategories.test.js is green

--- __test__/hookTest/apiTest/useHabitCategories.test.js
+++ __test__/hookTest/apiTest/useHabitCategories.test.js
@@ -1,114 +1,69 @@
 import React from "react";
 import { renderHook, act } from "@testing-library/react-hooks";
-import { waitFor } from "@testing-library/react"; // Cambiar importación aquí
-import { useDispatch } from "react-redux";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import useHabitCategories from "../../../hooks/api/useHabitCategories";
-import useCreateDefaultCategories from "../../../hooks/api/useCreateDefaultCategories";
-import { setHabitCategories } from "../../../actions/counterActions";
-import { logInfo } from "../../../util/logging";
-import useFetch from "../../../hooks/api/useFetch";
+import { waitFor } from "@testing-library/react-native";
 import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
+import { createStore } from "redux";
+import rootReducer from "../../../reducers/rootReducer";
+import useHabitCategories from "../../../hooks/api/useHabitCategories";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Mock de Axios
+// Mockear las dependencias
 jest.mock("axios");
-
-// Mock de AsyncStorage
-jest.mock("@react-native-async-storage/async-storage", () => ({
-  setItem: jest.fn().mockResolvedValue() // Simula una resolución exitosa
-}));
-
-// Mock de useDispatch
-const dispatchMock = jest.fn();
-jest.spyOn(require("react-redux"), "useDispatch").mockReturnValue(dispatchMock);
-
-// Mock de useFetch
-jest.mock("../../../hooks/api/useFetch");
-
-jest.mock("../../../hooks/api/useCreateDefaultCategories");
-jest.mock("../../../util/logging");
-
-const mockStore = configureStore(); // Crear el store mockeado
+jest.mock("@react-native-async-storage/async-storage");
 
 describe("useHabitCategories Hook", () => {
-  let dispatchMock;
-  let performFetchMock;
-  let cancelFetchMock;
-  let createCategoriesMock;
   let store;
 
+  const storedCredentials = {
+    name: "John Doe",
+    email: "johndoe@example.com"
+  };
+
+  const dataResponseWithCategories = {
+    success: true,
+    categories: [
+      { id: "1", name: "Fitness" },
+      { id: "2", name: "Wellness" }
+    ]
+  };
+
+  const dataResponseWithoutCategories = {
+    success: true,
+    categories: []
+  };
+
   beforeEach(() => {
-    dispatchMock = jest.fn();
-    useDispatch.mockReturnValue(dispatchMock);
-
-    createCategoriesMock = jest.fn().mockResolvedValue();
-    useCreateDefaultCategories.mockReturnValue(createCategoriesMock);
-
-    // Simulamos el comportamiento de useFetch
-    performFetchMock = jest.fn().mockResolvedValue(); // Simulamos la promesa resuelta
-    cancelFetchMock = jest.fn();
-    useFetch.mockReturnValue({
-      data: null,
-      error: null,
-      isLoading: false,
-      performFetch: performFetchMock,
-      cancelFetch: cancelFetchMock
+    store = createStore(rootReducer, {
+      habitCategoryIndex: { habitCategoryIndex: 0 }
     });
 
-    // Inicializamos el store mockeado
-    store = mockStore({
-      habitCategoryIndex: {
-        habitCategoryIndex: 0
-      }
-    });
+    // Mock de la implementación de axios para las respuestas por defecto
+    axios.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: dataResponseWithCategories
+      })
+    );
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should fetch and store categories when the hook is initialized", async () => {
-    const mockData = {
-      success: true,
-      categories: [
-        { id: "1", name: "Fitness" },
-        { id: "2", name: "Wellness" }
-      ]
-    };
-
-    // Mock de useFetch para simular la respuesta del servidor
-    useFetch.mockReturnValueOnce({
-      data: mockData,
-      error: null,
-      isLoading: false,
-      performFetch: jest.fn(),
-      cancelFetch: jest.fn()
+  it("should store categories in AsyncStorage when categories are found", async () => {
+    const { result } = renderHook(() => useHabitCategories(storedCredentials), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
     });
 
-    // Renderizar el hook con credenciales dentro del Provider
-    const credentials = { name: "John Doe", email: "johndoe@example.com" };
-
-    const { result } = renderHook(() => useHabitCategories(credentials), {
-      wrapper: ({ children }) => <Provider store={store}>{children}</Provider> // Envolvemos el hook con el Provider
-    });
-
-    // Actuar para llamar a performFetch
     await act(async () => {
       await result.current.fetchHabitCategories();
     });
 
-    // Verificar que AsyncStorage.setItem haya sido llamado correctamente
     await waitFor(() => {
       expect(AsyncStorage.setItem).toHaveBeenCalledWith(
         "habitCategories",
-        JSON.stringify(mockData.categories)
+        JSON.stringify(dataResponseWithCategories.categories)
       );
     });
-
-    // Verificar que se haya llamado a dispatch con las categorías
-    expect(dispatchMock).toHaveBeenCalledWith(
-      setHabitCategories(mockData.categories)
-    );
   });
 });
