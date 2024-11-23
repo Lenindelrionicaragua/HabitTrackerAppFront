dfe143e55f7f4011701b2d135470edcfa8cc349b
lenin del rio <lenindelrionicaragua@gmail.com>
2024-11-23 16:33:58 +0100

    update calculateDailyAverge.test.js

--- util/calculateDailyAverage.js
+++ util/calculateDailyAverage.js
@@ -1,10 +1,19 @@
 export function calculateDailyAverage(totalDailyMinutes) {
+  if (typeof totalDailyMinutes !== "object" || totalDailyMinutes === null) {
+    throw new Error("Invalid input: totalDailyMinutes must be an object.");
+  }
+
   const dates = Object.keys(totalDailyMinutes);
 
-  const totalMinutes = dates.reduce(
-    (sum, date) => sum + totalDailyMinutes[date],
-    0
-  );
+  const totalMinutes = dates.reduce((sum, date) => {
+    const value = totalDailyMinutes[date];
+    if (typeof value !== "number") {
+      throw new Error(
+        "Invalid input: totalDailyMinutes must contain numeric values."
+      );
+    }
+    return sum + value;
+  }, 0);
 
   const averageMinutes = totalMinutes / dates.length;
 
