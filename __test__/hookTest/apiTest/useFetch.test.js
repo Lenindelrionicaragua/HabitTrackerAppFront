c99fdf1138729bf828b1e52b5d1b52cf8e6f5d5d
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-06 01:39:44 +0200

    Remove Istanbul and coverage setup from the project

--- __test__/hookTest/apiTest/useFetch.test.js
+++ __test__/hookTest/apiTest/useFetch.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-native";
 import axios from "axios";
 import useFetch from "../../../hooks/api/useFetch";
 
