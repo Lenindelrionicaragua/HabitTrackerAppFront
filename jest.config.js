02dfe1b01c631fb457d62151f95685bb2fe24898
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-06 23:20:20 +0200

    chore(jest): remove coverage config now that Istanbul is no longer used

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   preset: "jest-expo",
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "**/*.{js,jsx}",
-    "!**/coverage/**",
-    "!**/node_modules/**",
-    "!**/babel.config.js",
-    "!**/jest.setup.js"
-  ],
   transformIgnorePatterns: [
     // "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
     "node_modules/(?!(jest-)?react-native|react-native|react-redux|@react-native|@react-navigation|@expo|expo|@unimodules|unimodules|sentry-expo|@reduxjs/toolkit|native-base|react-native-svg)"
