e4aba331b1d93b87b0b2ddbeccbda7017536ab6c
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-06 23:46:12 +0200

    Fix: Prevent coverage folder generation by disabling coverage collection in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverage: false,
   preset: "jest-expo",
   transformIgnorePatterns: [
     // "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
