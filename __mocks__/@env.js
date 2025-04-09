43ca12f521ec31d4b0cb9bc4c55963c6c466a6e0
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-09 23:24:43 +0200

    Add mock env file for testing

---
+++ __mocks__/@env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  EXPO_CLIENT_ID: "mock_expo_client_id",
+  IOS_CLIENT_ID: "mock_ios_client_id",
+  ANDROID_CLIENT_ID: "mock_android_client_id",
+  WEB_CLIENT_ID: "mock_web_client_id"
+};
