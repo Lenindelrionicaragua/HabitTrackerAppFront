363096e5f1c21bab3d6f452315710c412c22a402
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-10 00:24:25 +0200

    fix: Mock Alert globally in tests and restore mocks after each test

--- jest.setup.js
+++ jest.setup.js
@@ -2,10 +2,23 @@ import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/asy
 import Constants from "expo-constants";
 import { Alert } from "react-native";
 
+// Mock global de Alert
 jest.spyOn(Alert, "alert").mockImplementation(() => {});
 
+jest.mock("react-native", () => {
+  const original = jest.requireActual("react-native");
+  return {
+    ...original,
+    Alert: {
+      alert: jest.fn() // Mocking Alert.alert globally
+    }
+  };
+});
+
+// Mock de AsyncStorage
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 
+// Mock de Expo Constants
 jest.mock("expo-constants", () => {
   const actualConstants = jest.requireActual("expo-constants");
   return {
@@ -24,7 +37,3 @@ afterEach(() => {
 global.__expoConstants = Constants.manifest;
 
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
-
-afterEach(() => {
-  jest.restoreAllMocks();
-});
