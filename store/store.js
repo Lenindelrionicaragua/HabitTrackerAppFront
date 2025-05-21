ef67419c5e9c57d5f041c26fdd374c1706509ca0
Liz García <lizgarciamarin@gmail.com>
2025-05-21 12:26:32 +0200

    Updated middleware config, to avoud excessive warnings on dev mode.

--- store/store.js
+++ store/store.js
@@ -2,7 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../reducers/rootReducer";
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      immutableCheck: {
+        warnAfter: 200 // Raise threshold to 200ms, to avoid excessive warnings
+      }
+    })
 });
 
 export default store;
