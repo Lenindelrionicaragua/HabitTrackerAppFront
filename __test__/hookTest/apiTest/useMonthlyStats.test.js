2a343c1363ecd283c245c255aeabf7c1bde5882e
lenin del rio <lenindelrionicaragua@gmail.com>
2025-06-04 10:28:22 +0200

    test: mock getDaysInMonth returning 30 and use fake timers in useMonthlyStats tests

--- __test__/hookTest/apiTest/useMonthlyStats.test.js
+++ __test__/hookTest/apiTest/useMonthlyStats.test.js
@@ -14,7 +14,7 @@ jest.mock("axios");
 jest.mock("../../../util/logging");
 jest.mock("../../../util/roundingUtils", () => ({
   ...jest.requireActual("../../../util/roundingUtils"),
-  getDaysInMonth: () => 31 // Match the logic used in the hook
+  getDaysInMonth: () => 30 // Match the logic used in the hook
 }));
 
 describe("useMonthlyStats Hook - Success Case", () => {
