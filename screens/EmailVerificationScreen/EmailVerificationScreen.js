abd6efe2fb63c230e1d401d149927b5327992cc6
lenin del rio <lenindelrionicaragua@gmail.com>
2025-06-04 10:05:42 +0200

    fix: EmailVerification imports

--- screens/EmailVerificationScreen/EmailVerificationScreen.js
+++ screens/EmailVerificationScreen/EmailVerificationScreen.js
@@ -1,33 +1,67 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
-import axios from "axios";
+import useFetch from "../../hooks/api/useFetch";
+import {
+  StyledContainer,
+  TopContainer,
+  BottomContainer,
+  IconBackGround,
+  PageTitle,
+  InfoText
+} from "../EmailVerificationScreen/EmailVerificationScreenStyles";
+import { Octicons } from "@expo/vector-icons";
 
 const EmailVerificationScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { token } = route.params;
 
+  const handleResponse = data => {
+    if (data.success) {
+      navigation.replace("SuccessScreen");
+    } else {
+      navigation.replace("ErrorScreen", {
+        msg: data.msg || "Verification failed."
+      });
+    }
+  };
+
+  const { performFetch, isLoading, error } = useFetch(
+    "/auth/sign-up",
+    handleResponse
+  );
+
   useEffect(() => {
-    const verifyToken = async () => {
-      try {
-        const res = await axios.post("http://localhost:8081/auth/verify", {
-          token
-        });
-
-        if (res.data.success) {
-          navigation.replace("SuccessScreen");
-        } else {
-          navigation.replace("ErrorScreen", { msg: res.data.msg });
-        }
-      } catch (err) {
-        navigation.replace("ErrorScreen", { msg: err.message });
-      }
-    };
-
-    verifyToken();
+    if (token) {
+      performFetch({
+        method: "POST",
+        data: { token }
+      });
+    } else {
+      navigation.replace("ErrorScreen", { msg: "No token provided." });
+    }
   }, [token]);
 
-  return null;
+  return (
+    <StyledContainer>
+      <TopContainer>
+        <IconBackGround>
+          <Octicons name="mail" size={125} color="white" />
+        </IconBackGround>
+      </TopContainer>
+      <BottomContainer>
+        <PageTitle>Email Verification</PageTitle>
+        {isLoading && (
+          <>
+            <ActivityIndicator size="large" color="#000" />
+            <InfoText>Verifying your email, please wait...</InfoText>
+          </>
+        )}
+        {error && <InfoText style={{ color: "red" }}>{error.message}</InfoText>}
+      </BottomContainer>
+    </StyledContainer>
+  );
 };
 
 export default EmailVerificationScreen;
